721e74e9f7da33db5024c035115dacf37363c843
haoyilu <hlu72@wisc.edu>
2019-08-10 15:35:47 -0500

    CRUD finish

--- fullstack/Insta/views.py
+++ fullstack/Insta/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import TemplateView, ListView, DetailView
 from .models import Post
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 # Create your views here.
 class HelloDjango(TemplateView):
     template_name = 'home.html'
@@ -13,6 +14,17 @@ class PostDetail(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'make_post.html'
+    fields = '__all__'
 
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = "update_post.html"
+    fields = ['title']
 
-
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
